957fa0581cbdb0fea4abcdcaf058448417006de4
Bouteillepleine <76688492+Bouteillepleine@users.noreply.github.com>
2025-08-06 09:48:14 +0200

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Plus Kernel by TheWildJames or Morgan Weedman
+kernel.string=⚡Ultra⚡Kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -43,12 +43,91 @@ esac
 ui_print " " "  -> ksu_supported: $ksu_supported"
 $ksu_supported || abort "  -> Non-GKI device, abort."
 
-# boot install
-split_boot
-if [ -f "split_img/ramdisk.cpio" ]; then
-    unpack_ramdisk
-    write_boot
-else
-    flash_boot
+# Determine root method
+if [ -d /data/adb/magisk ] || [ -f /sbin/.magisk ]; then
+    ui_print "Magisk detected, current root method is Magisk. Flashing the KSU kernel in this case may brick your device, do you want to continue?"
+    ui_print "Please select an action:"
+    ui_print "Volume up key: No"
+    ui_print "Volume down button: Yes"
+    key_click=""
+    while [ "$key_click" = "" ]; do
+        key_click=$(getevent -qlc 1 | awk '{ print $3 }' | grep 'KEY_VOLUME')
+        sleep 0.2
+    done
+    case "$key_click" in
+        "KEY_VOLUMEUP") 
+            ui_print "You have chosen to exit the script"
+            ui_print "Exiting…"
+            exit 0
+            ;;
+        "KEY_VOLUMEDOWN")
+            ui_print "You have chosen to continue the installation"
+            ;;
+        *)
+            ui_print "Unknown key, exiting script"
+            exit 1
+            ;;
+    esac
 fi
-## end boot install
+
+ui_print "Starting ⚡Ultra⚡ kernel installation..."
+ui_print "Maintainer: ⚡Ultra⚡"
+ui_print "Features:"
+ui_print "-> SukiSu"
+ui_print "-> SUSFS v1.5.9 ඞ"
+ui_print "-> Wireguard Support"
+ui_print "-> Maphide LineageOS Detections"
+ui_print "-> Futile Maphide for jit-zygote-cache Detections"
+ui_print "-> Manual HOOK"
+ui_print "-> Magic Mount Support"
+ui_print "-> KPM"
+ui_print "-> BBR Support"
+if [ -L "/dev/block/bootdevice/by-name/init_boot_a" ] || [ -L "/dev/block/by-name/init_boot_a" ]; then
+    split_boot
+    flash_boot
+else
+    dump_boot
+    write_boot
+fi
+
+# Prioritize module path
+if [ -f "$AKHOME/ksu_module_susfs_1.5.2+_Release.zip" ]; then
+    MODULE_PATH="$AKHOME/ksu_module_susfs_1.5.2+_Release.zip"
+    ui_print "  -> Installing SUSFS Module from Release"
+elif [ -f "$AKHOME/ksu_module_susfs_1.5.2+_CI.zip" ]; then
+    MODULE_PATH="$AKHOME/ksu_module_susfs_1.5.2+_CI.zip"
+    ui_print "  -> Installing SUSFS Module from CI"
+else
+    ui_print "  -> No SUSFS Module found, Installing SUSFS Module from NON, Skipping Installation"
+    MODULE_PATH=""
+fi
+
+# Install SUSFS module (optional)
+if [ -n "$MODULE_PATH" ]; then
+    KSUD_PATH="/data/adb/ksud"
+    ui_print "Install SUSFS Module?"
+    ui_print "Volume UP: NO；Volume DOWN: YES"
+
+    key_click=""
+    while [ "$key_click" = "" ]; do
+        key_click=$(getevent -qlc 1 | awk '{ print $3 }' | grep 'KEY_VOLUME')
+        sleep 0.2
+    done
+    case "$key_click" in
+        "KEY_VOLUMEDOWN")
+            if [ -f "$KSUD_PATH" ]; then
+                ui_print "Installing SUSFS Module..."
+                /data/adb/ksud module install "$MODULE_PATH"
+                ui_print "Installation Complete"
+            else
+                ui_print "KSUD Not Found, Skipping Installation"
+            fi
+            ;;
+        "KEY_VOLUMEUP")
+            ui_print "Skipping SUSFS Module Installation"
+            ;;
+        *)
+            ui_print "Unknown Key Input, Skipping Installation"
+            ;;
+    esac
+fi
